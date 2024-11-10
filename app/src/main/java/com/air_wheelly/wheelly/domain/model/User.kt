610d7ac70b1699d8cfa4c79893d17ea08dfe1f27
fbudic21 <fbudic21@foi.hr>
2024-11-11 00:16:54 +0100

    SCRUM-19 Create repository class and interface

---
+++ app/src/main/java/com/air_wheelly/wheelly/domain/model/User.kt
@@ -0,0 +1,7 @@
+package com.air_wheelly.wheelly.domain.model
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val email: String
+)
---
+++ app/src/main/java/com/air_wheelly/wheelly/domain/repository/IAuthRepository.kt
@@ -0,0 +1,7 @@
+package com.air_wheelly.wheelly.domain.repository
+
+import com.air_wheelly.wheelly.domain.model.User
+
+interface IAuthRepository {
+    suspend fun registerUser(firstName: String, lastName: String, email: String, password: String) : User
+}
