dafd0d36d4dfa8bc4ee505e281eb4af0ce6d8508
Mirza <mblagajce21@student.foi.hr>
2024-11-17 13:28:42 +0100

    SCRUM-20 Implement Login repository

--- app/src/main/java/com/air_wheelly/wheelly/domain/model/User.kt
+++ app/src/main/java/com/air_wheelly/wheelly/domain/model/User.kt
@@ -1,7 +1,8 @@
 package com.air_wheelly.wheelly.domain.model
 
 data class User(
-    val firstName: String,
-    val lastName: String,
-    val email: String
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val email: String? = null,
+    val jwt: String? = null
 )
