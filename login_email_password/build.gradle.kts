cacc3dfbf8bc064c7658ddcafa5bccbf73b92aa7
Mirza <mblagajce21@student.foi.hr>
2024-12-06 23:57:57 +0100

    SCRUM-39 initial build gradle commit

--- login_email_password/build.gradle.kts
+++ login_email_password/build.gradle.kts
@@ -4,6 +4,13 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+kotlin {
+    jvmToolchain(8)
+}
+dependencies {
+    implementation(project(mapOf("path" to ":core")))
+    implementation(project(mapOf("path" to ":ws")))
 }
