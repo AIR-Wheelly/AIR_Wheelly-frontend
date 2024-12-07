aeef1e68552056abc8c719af0ac17704e7ccd762
Mirza <mblagajce21@student.foi.hr>
2024-12-07 16:05:32 +0100

    SCRUM-39 Fix bug preventing loging in

--- core/src/main/java/hr/air_wheelly/core/network/models/SuccessfulResponseBody.kt
+++ core/src/main/java/hr/air_wheelly/core/network/models/SuccessfulResponseBody.kt
@@ -1,3 +1,3 @@
 package hr.air_wheelly.core.network.models
 
-class SuccessfulResponseBody<T>(success: Boolean, message: String, val data: Array<T>) : ResponseBody(success, message)
+class SuccessfulResponseBody<T>(success: Boolean, message: String, val token : String) : ResponseBody(success, message)
--- ws/src/main/java/hr/air_wheelly/ws/network/NetworkService.kt
+++ ws/src/main/java/hr/air_wheelly/ws/network/NetworkService.kt
@@ -3,7 +3,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkService {
-    private const val BASE_URL = "http://127.0.0.1:8080/api/"
+    private const val BASE_URL = "http://10.0.2.2:8080/api/"
 
     private var instance: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
