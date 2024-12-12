dc5778ba504256dcd6ab362a9e3a413c4d673ad8
fbudic21 <fbudic21@foi.hr>
2024-12-12 12:40:09 +0100

    SCRUM-40 Add Android Library module for Google login

--- login_google/build.gradle.kts
+++ login_google/build.gradle.kts
@@ -1,21 +1,46 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+android {
+    namespace = "hr.air_wheelly.login_google"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 34
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
-kotlin {
-    jvmToolchain(8)
-}
 dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(project(mapOf("path" to ":core")))
     implementation(project(mapOf("path" to ":ws")))
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    //Google oAuth
+    //oAuth
     implementation("androidx.credentials:credentials:1.3.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
 
--- login_google/src/main/java/hr/air_wheelly/login_google/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package hr.air_wheelly.login_google
-
-class MyClass {
-}
