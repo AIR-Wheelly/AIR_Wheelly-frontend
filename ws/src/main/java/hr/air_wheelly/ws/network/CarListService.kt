4aa7ed45256bac78fd3e4ac7ae2c2930db1cfae0
fbudic21 <fbudic21@foi.hr>
2024-12-13 15:09:23 +0100

    SCRUM-50 Create Service for cars

---
+++ ws/src/main/java/hr/air_wheelly/ws/network/CarListService.kt
@@ -0,0 +1,12 @@
+package hr.air_wheelly.ws.network
+
+import hr.air_wheelly.core.network.models.SuccessfulResponseBody
+import hr.air_wheelly.ws.models.responses.CarListResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface CarListService {
+
+    @GET("car")
+    fun getListedCars() : Call<SuccessfulResponseBody<List<CarListResponse>>>
+}
