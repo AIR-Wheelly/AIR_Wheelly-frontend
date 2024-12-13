9dcddb4ab36df93d54e76471467756c42f305221
Mirza <mblagajce21@student.foi.hr>
2024-12-13 15:10:30 +0100

    SCRUM-51 Fix modules to use android

--- ws/build.gradle.kts
+++ ws/build.gradle.kts
@@ -1,15 +1,26 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+android {
+    namespace = "com.air_wheelly.wheelly.ws"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
-kotlin {
-    jvmToolchain(8)
-}
+
 dependencies {
     implementation(project(mapOf("path" to ":core")))
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
