de1dbb0a66b5aaea7dc01531ae9b8831510e1271
Mirza <mblagajce21@student.foi.hr>
2024-12-15 23:05:50 +0100

    SCRUM-51 Enable location services module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,5 @@ dependencies {
 
     // NAVIGATION
     implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
