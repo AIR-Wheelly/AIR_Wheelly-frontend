ddf3091ac39e81a4d1d9df6230a8bcd13da5ea49
fbudic21 <fbudic21@foi.hr>
2025-01-10 23:37:33 +0100

    SCRUM-56 add navigation to car list screen

--- app/src/main/java/com/air_wheelly/wheelly/presentation/car_list/CarList.kt
+++ app/src/main/java/com/air_wheelly/wheelly/presentation/car_list/CarList.kt
@@ -63,136 +63,140 @@ fun CarList(
         carListHandler.fetchCarList()
     }
 
-    BottomSheetScaffold(
-        scaffoldState = scaffoldState,
-        drawerElevation = 100.dp,
-        sheetElevation = 100.dp,
-        sheetContent = {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                Divider(
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally),
-                    color = Color.DarkGray,
-                    thickness = 4.dp,
-                )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp)
+    ) {
+        Box(
+            modifier = Modifier.weight(1f)
+        ) {
+            BottomSheetScaffold(
+                scaffoldState = scaffoldState,
+                drawerElevation = 100.dp,
+                sheetElevation = 100.dp,
+                sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+                sheetBackgroundColor = MaterialTheme.colorScheme.inverseOnSurface,
+                sheetPeekHeight = 55.dp,
+                sheetGesturesEnabled = true,
+                sheetContent = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        Divider(
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally),
+                            color = Color.DarkGray,
+                            thickness = 4.dp,
+                        )
 
-                Text(
-                    "Filter Options",
-                    style = MaterialTheme.typography.headlineSmall,
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                )
+                        Text(
+                            "Filter Options",
+                            style = MaterialTheme.typography.headlineSmall,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                        )
 
-                Spacer(modifier = Modifier.height(8.dp))
+                        Spacer(modifier = Modifier.height(8.dp))
 
-                Text("Fuel Type", style = MaterialTheme.typography.bodyLarge)
-                FlowRow(
-                    modifier = Modifier.fillMaxWidth(),
-                    maxItemsInEachRow = 2,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    EnumFuelType.values().forEach { fuelType ->
-                        Button(
-                            modifier = Modifier.fillMaxWidth(0.45f),
-                            onClick = {
-                                tempSelectedFuelType = if (tempSelectedFuelType.contains(fuelType)) tempSelectedFuelType - fuelType else tempSelectedFuelType + fuelType
-                            },
-                            colors = ButtonDefaults.buttonColors(
-                                backgroundColor = if (tempSelectedFuelType.contains(fuelType)) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary
-                            )
+                        Text("Fuel Type", style = MaterialTheme.typography.bodyLarge)
+                        FlowRow(
+                            modifier = Modifier.fillMaxWidth(),
+                            maxItemsInEachRow = 2,
+                            horizontalArrangement = Arrangement.SpaceBetween
                         ) {
-                            Text(fuelType.name)
+                            EnumFuelType.values().forEach { fuelType ->
+                                Button(
+                                    modifier = Modifier.fillMaxWidth(0.45f),
+                                    onClick = {
+                                        tempSelectedFuelType = if (tempSelectedFuelType.contains(fuelType)) tempSelectedFuelType - fuelType else tempSelectedFuelType + fuelType
+                                    },
+                                    colors = ButtonDefaults.buttonColors(
+                                        backgroundColor = if (tempSelectedFuelType.contains(fuelType)) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary
+                                    )
+                                ) {
+                                    Text(fuelType.name)
+                                }
+                            }
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Text("Manufacturer", style = MaterialTheme.typography.bodyLarge)
+                        TextField(
+                            value = tempSelectedManufacturer,
+                            onValueChange = { tempSelectedManufacturer = it },
+                            label = { Text("Manufacturer") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Text("Year", style = MaterialTheme.typography.bodyLarge)
+                        TextField(
+                            value = tempSelectedYear?.toString() ?: "",
+                            onValueChange = { tempSelectedYear = it.toIntOrNull() },
+                            label = { Text("Year") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                selectedFuelType = tempSelectedFuelType
+                                selectedManufacturer = tempSelectedManufacturer
+                                selectedYear = tempSelectedYear
+                            }
+                        ) {
+                            Text("Apply Filters")
+                        }
+
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                selectedFuelType = EnumFuelType.values().toSet()
+                                selectedManufacturer = ""
+                                selectedYear = null
+                                tempSelectedFuelType = EnumFuelType.values().toSet()
+                                tempSelectedManufacturer = ""
+                                tempSelectedYear = null
+                            }
+                        ) {
+                            Text("Clear Filters")
                         }
                     }
                 }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Text("Manufacturer", style = MaterialTheme.typography.bodyLarge)
-                TextField(
-                    value = tempSelectedManufacturer,
-                    onValueChange = { tempSelectedManufacturer = it },
-                    label = { Text("Manufacturer") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Text("Year", style = MaterialTheme.typography.bodyLarge)
-                TextField(
-                    value = tempSelectedYear?.toString() ?: "",
-                    onValueChange = { tempSelectedYear = it.toIntOrNull() },
-                    label = { Text("Year") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = {
-                        selectedFuelType = tempSelectedFuelType
-                        selectedManufacturer = tempSelectedManufacturer
-                        selectedYear = tempSelectedYear
-                    }
-                ) {
-                    Text("Apply Filters")
-                }
-
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = {
-                        selectedFuelType = EnumFuelType.values().toSet()
-                        selectedManufacturer = ""
-                        selectedYear = null
-                        tempSelectedFuelType = EnumFuelType.values().toSet()
-                        tempSelectedManufacturer = ""
-                        tempSelectedYear = null
-                    }
-                ) {
-                    Text("Clear Filters")
-                }
-            }
-        },
-        sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
-        sheetPeekHeight = 40.dp,
-        sheetGesturesEnabled = true
-    ) {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                if (errorMessage != null) {
-                    Text("Error: $errorMessage", color = Color.Red)
-                } else {
-                    filteredCarList.forEach { car ->
-                        CarCard(car)
-                    }
-                }
-
-                Spacer(modifier = Modifier.weight(1f))
-
-                Button(
-                    onClick = {
-                        navController.navigate("createListing")
-                    },
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .padding(bottom = 30.dp)
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
                 ) {
-                    Text("List Your Car")
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(bottom = 0.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        if (errorMessage != null) {
+                            Text("Error: $errorMessage", color = Color.Red)
+                        } else {
+                            filteredCarList.forEach { car ->
+                                CarCard(car)
+                            }
+                        }
+                    }
                 }
             }
         }
+
+        com.air_wheelly.wheelly.presentation.components.BottomNavigation(
+            navController = navController,
+            modifier = Modifier
+                .padding(start = 0.dp, end = 0.dp)
+                .fillMaxWidth()
+        )
     }
 }
