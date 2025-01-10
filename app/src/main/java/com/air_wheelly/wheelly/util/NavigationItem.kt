d8257dbae83ee719e78572ccd4b7bab5c90c3dc9
fbudic21 <fbudic21@foi.hr>
2025-01-10 18:54:48 +0100

    SCRUM-56 create navigation item objects

---
+++ app/src/main/java/com/air_wheelly/wheelly/util/NavigationItem.kt
@@ -0,0 +1,10 @@
+package com.air_wheelly.wheelly.util
+
+import com.air_wheelly.wheelly.R
+
+sealed class NavigationItem(val route: String, val icon: Int, val title: Int) {
+    object Home : NavigationItem("carList", R.drawable.baseline_home_24, R.string.home)
+    object Profile : NavigationItem("profile", R.drawable.baseline_person_2_24, R.string.profile)
+    object Chat : NavigationItem("chat", R.drawable.baseline_chat_24, R.string.chat)
+    object History : NavigationItem("history", R.drawable.baseline_history_24, R.string.history)
+}
