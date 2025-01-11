a8b5ca96ff132ce3b6cff21fa465e5a22727ceda
fbudic21 <fbudic21@foi.hr>
2025-01-11 23:56:04 +0100

    SCRUM-8 display reservations

--- app/src/main/java/com/air_wheelly/wheelly/presentation/reservations/ReservationHistoryScreen.kt
+++ app/src/main/java/com/air_wheelly/wheelly/presentation/reservations/ReservationHistoryScreen.kt
@@ -1,15 +1,22 @@
 package com.air_wheelly.wheelly.presentation.reservations
 
 import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.air_wheelly.wheelly.presentation.components.ReservationCard
 import hr.air_wheelly.core.network.ResponseListener
 import hr.air_wheelly.core.network.models.ErrorResponseBody
 import hr.air_wheelly.core.network.models.SuccessfulResponseBody
 import hr.air_wheelly.ws.models.responses.reservation.PastReservationsResponse
 import hr.air_wheelly.ws.request_handlers.PastReservationsRequestHandler
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ReservationHistoryScreen(
     navController: NavController
@@ -34,4 +41,35 @@ fun ReservationHistoryScreen(
             }
         }
     )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp)
+    ){
+        Box(
+            modifier = Modifier.weight(1f)
+        ) {
+            Surface(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 16.dp, end = 16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    pastReservations?.forEach { reservation ->
+                        ReservationCard(reservation)
+                    }
+                }
+            }
+        }
+        com.air_wheelly.wheelly.presentation.components.BottomNavigation(
+            navController = navController,
+            modifier = Modifier
+                .padding(start = 0.dp, end = 0.dp)
+                .fillMaxWidth()
+        )
+    }
 }
