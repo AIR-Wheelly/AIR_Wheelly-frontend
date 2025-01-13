bcf770c1d0edce735043934d31bfcb6b2aaa684f
LeoCavar <126581280+lcavar21@users.noreply.github.com>
2025-01-13 06:24:25 +0100

    SCRUM-43 Add @Body annotation to updateProfileRequest in EditProfileService

--- ws/src/main/java/hr/air_wheelly/ws/network/EditProfileService.kt
+++ ws/src/main/java/hr/air_wheelly/ws/network/EditProfileService.kt
@@ -1,21 +1,16 @@
 package hr.air_wheelly.ws.network
+
 import hr.air_wheelly.core.network.models.SuccessfulResponseBody
-import hr.air_wheelly.ws.models.LoggedInUserJWT
-import hr.air_wheelly.ws.models.RegistrationBody
+import hr.air_wheelly.ws.models.responses.UserProfileResponse
+import hr.air_wheelly.ws.models.responses.UpdateProfileResponse
 import hr.air_wheelly.ws.models.UpdateProfileRequest
-import hr.air_wheelly.ws.models.responses.*
 import retrofit2.Call
 import retrofit2.http.*
 
 interface EditProfileService {
     @GET("auth/profile")
-    fun getUserProfile(
-        @Header("Authorization") token: String // Authorization token in header
-    ): Call<SuccessfulResponseBody<UserProfileResponse>>
+    fun getUserProfile(): Call<SuccessfulResponseBody<UserProfileResponse>>
 
     @PUT("auth/updateProfile")
-    fun updateUserProfile(
-        @Header("Authorization") token: String, // Authorization token in header
-        @Body updateProfileRequest: UpdateProfileRequest
-    ): Call<SuccessfulResponseBody<UpdateProfileResponse>>
+    fun updateUserProfile(@Body updateProfileRequest: UpdateProfileRequest): Call<SuccessfulResponseBody<UpdateProfileResponse>>
 }
