1a158f2be4daeb2dd959452b7469f4b5a663c9fe
Mirza <mblagajce21@student.foi.hr>
2025-01-13 19:15:29 +0100

    SCRUM-59 Change width of photo to be full width

--- app/src/main/java/com/air_wheelly/wheelly/presentation/components/Base64Image.kt
+++ app/src/main/java/com/air_wheelly/wheelly/presentation/components/Base64Image.kt
@@ -3,7 +3,10 @@ package com.air_wheelly.wheelly.presentation.components
 import android.graphics.BitmapFactory
 import android.util.Base64
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import java.io.ByteArrayInputStream
@@ -11,17 +14,23 @@ import java.io.ByteArrayInputStream
 @Composable
 fun Base64Image(
     base64String: String?,
-    modifier: Modifier
+    modifier: Modifier = Modifier
 ) {
-    val decodedString = Base64.decode(base64String, Base64.DEFAULT)
-    val inputStream = ByteArrayInputStream(decodedString)
-    val bitmap = BitmapFactory.decodeStream(inputStream)
+    val decodedString = remember(base64String) { Base64.decode(base64String, Base64.DEFAULT) }
+    val bitmap = remember(decodedString) {
+        val inputStream = ByteArrayInputStream(decodedString)
+        BitmapFactory.decodeStream(inputStream)
+    }
 
     if (bitmap != null) {
+        val aspectRatio = bitmap.width.toFloat() / bitmap.height.toFloat()
+
         Image(
             bitmap = bitmap.asImageBitmap(),
             contentDescription = "Car Image",
             modifier = modifier
+                .fillMaxWidth()
+                .aspectRatio(aspectRatio)
         )
     }
 }
