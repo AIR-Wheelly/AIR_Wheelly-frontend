8c032bd78f8f4d0c68a398f2de11fc4de3a12f27
fbudic21 <fbudic21@foi.hr>
2025-01-22 13:19:35 +0100

    SCRUM-9 implement callback functions

--- app/src/main/java/com/air_wheelly/wheelly/domain/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/air_wheelly/wheelly/domain/statistics/StatisticsViewModel.kt
@@ -7,8 +7,10 @@ import androidx.lifecycle.viewModelScope
 import hr.air_wheelly.core.network.ResponseListener
 import hr.air_wheelly.core.network.models.ErrorResponseBody
 import hr.air_wheelly.core.network.models.SuccessfulResponseBody
+import hr.air_wheelly.ws.models.responses.statistics.LastMonthResponse
 import hr.air_wheelly.ws.models.responses.statistics.NumberOfRentsPerCarResponse
 import hr.air_wheelly.ws.request_handlers.StatisticsGetRentsPerCarRequestHandler
+import hr.air_wheelly.ws.request_handlers.StatisticsLastMonthRequestHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -33,6 +35,45 @@ class StatisticsViewModel(
                     override fun onSuccessfulResponse(response: SuccessfulResponseBody<List<NumberOfRentsPerCarResponse>>) {
                         _state.value = _state.value.copy(
                             numberOfRentsPerCar = response.result,
+                            isLoading = false,
+                        )
+                        Log.d("STATISTICS", "Result: " + response.result.toString())
+                    }
+
+                    override fun onErrorResponse(response: ErrorResponseBody) {
+                        _state.value = _state.value.copy(
+                            errorMessage = response.error_message,
+                            isLoading = false
+                        )
+                        Log.d("STATISTICS", "Error: " + response.error_message)
+                    }
+
+                    override fun onNetworkFailure(t: Throwable) {
+                        _state.value = _state.value.copy(
+                            errorMessage = "Network failure, please try again later",
+                            isLoading = false
+                        )
+                        Log.d("STATISTICS", "Error: " + t.cause)
+                    }
+                }
+            )
+        }
+    }
+
+    fun fetchLastMonth() {
+        val handler = StatisticsLastMonthRequestHandler(context)
+
+        viewModelScope.launch {
+            _state.value = _state.value.copy(
+                isLoading = true,
+                errorMessage = null
+            )
+
+            handler.sendRequest(
+                object : ResponseListener<LastMonthResponse> {
+                    override fun onSuccessfulResponse(response: SuccessfulResponseBody<LastMonthResponse>) {
+                        _state.value = _state.value.copy(
+                            lastMonth = response.result,
                             isLoading = false,
                         )
                         Log.d("STATISTICS", "Result: " + response.result.toString())
