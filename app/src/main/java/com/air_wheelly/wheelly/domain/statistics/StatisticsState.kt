8d1ecbb614d63a101830b5e22595a2c4ca26920d
fbudic21 <fbudic21@foi.hr>
2025-01-22 13:13:00 +0100

    SCRUM-9 add state field for response

--- app/src/main/java/com/air_wheelly/wheelly/domain/statistics/StatisticsState.kt
+++ app/src/main/java/com/air_wheelly/wheelly/domain/statistics/StatisticsState.kt
@@ -1,9 +1,10 @@
 package com.air_wheelly.wheelly.domain.statistics
 
 import com.air_wheelly.wheelly.domain.BaseState
+import hr.air_wheelly.ws.models.responses.statistics.NumberOfRentsPerCarResponse
 
 data class StatisticsState(
-
+    val numberOfRentsPerCar: List<NumberOfRentsPerCarResponse> = emptyList(),
     override val isLoading: Boolean = false,
     override val errorMessage: String? = null
 ) : BaseState(isLoading, errorMessage)
