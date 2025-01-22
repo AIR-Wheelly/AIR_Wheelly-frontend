0c4bcd6b696921e9ca479584a710f6c953f7c8c6
fbudic21 <fbudic21@foi.hr>
2025-01-22 13:38:24 +0100

    SCRUM-9 display statistics

--- app/src/main/java/com/air_wheelly/wheelly/presentation/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/air_wheelly/wheelly/presentation/statistics/StatisticsScreen.kt
@@ -1,13 +1,18 @@
 package com.air_wheelly.wheelly.presentation.statistics
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.air_wheelly.wheelly.domain.statistics.StatisticsViewModel
 import com.air_wheelly.wheelly.domain.statistics.StatisticsViewModelFactory
+import com.air_wheelly.wheelly.presentation.components.CarRentStatisticsCard
 
 @Composable
 fun StatisticsScreen(
@@ -17,5 +22,33 @@ fun StatisticsScreen(
     val viewModel: StatisticsViewModel = viewModel(factory = StatisticsViewModelFactory(context))
     val state by viewModel.state.collectAsState()
 
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp)
+    ) {
+        Box(
+            modifier = Modifier.weight(1f)
+        ) {
+            Surface(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 16.dp, end = 16.dp)
+                ) {
+                    state.numberOfRentsPerCar.forEach { numberOfRents ->
+                        CarRentStatisticsCard(numberOfRents)
+                    }
+                }
+            }
+        }
+        com.air_wheelly.wheelly.presentation.components.BottomNavigation(
+            navController = navController,
+            modifier = Modifier
+                .padding(start = 0.dp, end = 0.dp)
+                .fillMaxWidth()
+        )
+    }
 }
