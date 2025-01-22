6af6242643ac17d377b1d52c26f1cb893ebd396e
fbudic21 <fbudic21@foi.hr>
2025-01-22 12:39:38 +0100

    SCRUM-9 initialize state and view model

--- app/src/main/java/com/air_wheelly/wheelly/domain/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/air_wheelly/wheelly/domain/statistics/StatisticsViewModel.kt
@@ -2,9 +2,14 @@ package com.air_wheelly.wheelly.domain.statistics
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class StatisticsViewModel(
     private val context: Context
 ) : ViewModel() {
+    private val _state = MutableStateFlow(StatisticsState())
+    val state: StateFlow<StatisticsState> = _state
+
 
 }
--- app/src/main/java/com/air_wheelly/wheelly/presentation/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/air_wheelly/wheelly/presentation/statistics/StatisticsScreen.kt
@@ -1,8 +1,21 @@
 package com.air_wheelly.wheelly.presentation.statistics
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.air_wheelly.wheelly.domain.statistics.StatisticsViewModel
+import com.air_wheelly.wheelly.domain.statistics.StatisticsViewModelFactory
 
 @Composable
-fun StatisticsScreen() {
+fun StatisticsScreen(
+    navController: NavController
+) {
+    val context = LocalContext.current
+    val viewModel: StatisticsViewModel = viewModel(factory = StatisticsViewModelFactory(context))
+    val state by viewModel.state.collectAsState()
+
 
 }
