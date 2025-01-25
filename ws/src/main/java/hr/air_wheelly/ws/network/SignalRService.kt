dd621ba350245be4d6a56edd6f86a41d6ba9e3d3
Mirza <mblagajce21@student.foi.hr>
2025-01-25 21:55:39 +0100

    SCRUM-71 Use chat listener for signal r

--- ws/src/main/java/hr/air_wheelly/ws/network/SignalRService.kt
+++ ws/src/main/java/hr/air_wheelly/ws/network/SignalRService.kt
@@ -1,6 +1,7 @@
 package hr.air_wheelly.ws.network
 
 import android.content.Context
+import android.util.Log
 import com.microsoft.signalr.HubConnection
 import com.microsoft.signalr.HubConnectionBuilder
 import hr.air_wheelly.ws.models.ChatMessage
@@ -10,6 +11,7 @@ import kotlinx.coroutines.withContext
 
 class SignalRService(private val context: Context, private val reservationId: String) {
     private lateinit var hubConnection: HubConnection
+    private var onMessageReceived: ((ChatMessage) -> Unit)? = null
 
     fun startConnection() {
         val token = TokenManager.getToken(context)
@@ -18,20 +20,23 @@ class SignalRService(private val context: Context, private val reservationId: St
             .build()
 
         hubConnection.on("ReceiveMessage", { message: ChatMessage ->
-
+            Log.d("SignalR", "Received message: $message")
+            onMessageReceived?.invoke(message)
         }, ChatMessage::class.java)
 
         hubConnection.start().blockingAwait()
         hubConnection.send("JoinGroup", reservationId)
     }
 
-    suspend fun sendMessage(message: String) {
-        withContext(Dispatchers.IO) {
-            hubConnection.send("SendMessage", reservationId, message)
-        }
+    fun sendMessage(message: String) {
+        hubConnection.send("SendMessage", reservationId, message)
     }
 
     fun stopConnection() {
         hubConnection.stop().blockingAwait()
+    }
+
+    fun setOnMessageReceivedListener(listener: (ChatMessage) -> Unit) {
+        onMessageReceived = listener
     }
 }
