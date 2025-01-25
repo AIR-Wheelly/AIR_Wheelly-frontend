5202e5e89d2c76ac0edd5bbd7587b9c8bee8e614
Mirza <mblagajce21@student.foi.hr>
2025-01-25 18:56:22 +0100

    SCRUM-70 Fix initial chat screen

--- app/src/main/java/com/air_wheelly/wheelly/presentation/chat/ChatScreen.kt
+++ app/src/main/java/com/air_wheelly/wheelly/presentation/chat/ChatScreen.kt
@@ -1,20 +1,23 @@
 package com.air_wheelly.wheelly.presentation.chat
 
+import android.content.Context
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.air_wheelly.wheelly.domain.chat.ChatViewModel
+import com.air_wheelly.wheelly.presentation.viewmodel.ChatViewModel
 import com.air_wheelly.wheelly.domain.chat.ChatViewModelFactory
 import hr.air_wheelly.ws.models.ChatMessage
 
 @Composable
 fun ChatScreen(reservationId: String) {
-    val viewModel: ChatViewModel = viewModel(factory = ChatViewModelFactory(reservationId))
+    val context = LocalContext.current
+    val viewModel: ChatViewModel = viewModel(factory = ChatViewModelFactory(context, reservationId))
     val chatMessages by viewModel.chatMessages.collectAsState()
     var message by remember { mutableStateOf("") }
 
