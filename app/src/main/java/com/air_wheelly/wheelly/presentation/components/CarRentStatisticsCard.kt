557df72d7c2f7e29d91d5c7fdc6cc077e6189812
fbudic21 <fbudic21@foi.hr>
2025-02-02 00:19:21 +0100

    SCRUM-77 change to Elevated Card

--- app/src/main/java/com/air_wheelly/wheelly/presentation/components/CarRentStatisticsCard.kt
+++ app/src/main/java/com/air_wheelly/wheelly/presentation/components/CarRentStatisticsCard.kt
@@ -2,8 +2,8 @@ package com.air_wheelly.wheelly.presentation.components
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +14,7 @@ import hr.air_wheelly.ws.models.responses.statistics.NumberOfRentsPerCarResponse
 
 @Composable
 fun CarRentStatisticsCard(numberOfRentsPerCar: NumberOfRentsPerCarResponse) {
-    Card(
+    ElevatedCard(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
